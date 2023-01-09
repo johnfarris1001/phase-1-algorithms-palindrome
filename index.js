a48fa8077663f1b45f78517af4033d9989b1bdd9
John <johnfarris217@gmail.com>
2023-01-08 22:33:47 -0600

    commit tree 384651cbe7ec9e3d002051acccff2a2c91a78b7e

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversed = '';
+  for (let i = 0; i < word.length; i++) {
+    reversed = word[i] + reversed;
+  }
+  return word === reversed;
 }
 
 /* 
   Add your pseudocode here
+  declare an empty string
+  iterate through the word and add each letter to the beginning of the empty string
+  check to see if the string matches the word
 */
 
 /*
   Add written explanation of your solution here
+  Use concatenation to rebuild the word backwards, check to see if it matches
 */
 
 // You can run `node index.js` to view these console logs
